2ce049d50e0a5b906ffa3458f4250cf4f4827bb8
Gabriel Revells <gcrevell@mtu.edu>
2016-08-11 09:42:44 -0400

    Initial commit

--- voltRelax/ViewController.swift
+++ voltRelax/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var backgroundMusic = AVAudioPlayer()
+    
+    func setupAudioPlayerWithFile(_ file:NSString, type:NSString) -> AVAudioPlayer  {
+        //1
+        let path = Bundle.main.path(forResource: file as String, ofType: type as String)
+        let url = URL(fileURLWithPath: path!)
+        
+        //3
+        var audioPlayer:AVAudioPlayer?
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+        } catch let error1 as NSError {
+            audioPlayer = nil
+        }
+        
+        //4
+        return audioPlayer!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        backgroundMusic = self.setupAudioPlayerWithFile("relax", type:"mp3")
+        backgroundMusic.volume = 0.3
+        backgroundMusic.play()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
